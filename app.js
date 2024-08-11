8bd15358bdbe6ff44e162572f290c798b4adcdc5
Sameer Baluch <balochtradersnayapay@gmail.com>
2024-08-11 14:09:55 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 var words = ["bat", "tap", "cat", "tab", "pat"];
-var anagramWords = [[]]
-
-
-let anagram = (a, b) => {
-    return a.split('').sort().join('') == b.split('').sort().join('')
-}
+var anagramWords = []
 
 for (let i = 0; i < words.length; i++) {
     for (let j = i + 1; j < words.length; j++) {
-        if (anagram(words[i], words[j])) {
+        var sorted = words[i].split('').sort().join('') == words[j].split('').sort().join('')
+        if (sorted) {
             anagramWords.push([words[i], words[j]])
         }
     }
 }
-
 console.log(anagramWords);
-
-
